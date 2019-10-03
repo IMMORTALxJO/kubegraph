6d84d0f4ed7d3827a5ea8d5687fa10bc54c08b80
IMMORTALxJO <iblaaaz@gmail.com>
2019-10-03 21:24:21 +0300

    Init commit

---
+++ src/utils.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+from difflib import SequenceMatcher
+
+# compare strings
+def compare_strings(a, b, cache={}):
+  cache_key = (a, b)
+  if cache_key in cache:
+    return cache[cache_key]
+  left_ratio = SequenceMatcher(None, a, b).ratio()
+  right_ratio = SequenceMatcher(None, a[::-1], b[::-1]).ratio()
+  good_ratio = (1 - 3 / (len(a) + len(b)))
+  ret = (left_ratio + right_ratio) / 2 >= good_ratio
+  cache[cache_key] = ret
+  return ret
+
+# join strings
+def join_strings(*args, cache={}):
+  cache_key = (args,)
+  if cache_key in cache:
+    return cache[cache_key]
+
+  args = [str(arg) for arg in args if arg != '']
+
+  args = sorted(args, key=len, reverse=True)
+
+  left = ''
+  left_padding = 0
+  while left_padding < len(args[0]) and all(left_padding < len(arg) and arg[left_padding] == args[0][left_padding] for arg in args):
+    left += args[0][left_padding]
+    left_padding += 1
+
+  if left == args[0]:
+    cache[cache_key] = left
+    return left
+
+  right = ''
+  right_padding = -1
+  while all(arg[right_padding] == args[0][right_padding] for arg in args):
+    right = args[0][right_padding] + right
+    right_padding -= 1
+  right_padding += 1
+
+  diff = [arg[left_padding:(len(arg) + right_padding)] for arg in args]
+  diff = sorted((set(diff)))
+
+  ret = '%s{%s}%s' % (left, str.join(',', diff), right)
+  cache[cache_key] = ret
+  return ret
+
+
+def group_similars(*args):
+  skip = set()
+  groups = list()
+  for a in args:
+    if a in skip:
+      continue
+    group = set()
+    skip.add(a)
+    group.add(a)
+    for b in args:
+      if b in skip:
+        continue
+      if compare_strings(a, b):
+        skip.add(b)
+        group.add(b)
+    groups.append(group)
+  return groups
+
+
